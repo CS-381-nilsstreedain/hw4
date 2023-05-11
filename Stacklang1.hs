c985db5a64d00515f5da360ec5aa45bcb69af76a
Nils Streedain <nils@streedain.com>
2023-05-11 16:16:15 -0700

    Commented stacklangs

--- Stacklang1.hs
+++ Stacklang1.hs
@@ -1,25 +1,30 @@
 module Stacklang1 where
 
+-- Define the type for a program
 type Prog = [Cmd]
-data Cmd = LD Int | ADD | MULT | DUP deriving Show
+
+-- Define the data type for commands in the stack language
+data Cmd
+    = LD Int	-- Load int onto stack
+    | ADD		-- Add top 2 ints on stack
+    | MULT		-- Multiply top 2 ints on stack
+    | DUP		-- Duplicate top element on stack
+    deriving Show
+
+-- Define the type for the stack
 type Stack = [Int]
 
+-- Function to run a program on a given stack
 run :: Prog -> Stack -> Maybe Stack
-run [] s = Just s
-run (c:cs) s = semCmd c s >>= run cs
+run [] s = Just s						-- If no commands left, return stack
+run (c:cs) s = semCmd c s >>= run cs	-- Execute cmd, continue with remaining
 
+-- Function to execute individual commands
 semCmd :: Cmd -> Stack -> Maybe Stack
-semCmd (LD n) s = Just (n : s)
-semCmd ADD (x:y:xs) = Just (x + y : xs)
-semCmd ADD _ = Nothing
-semCmd MULT (x:y:xs) = Just (x * y : xs)
-semCmd MULT _ = Nothing
-semCmd DUP (x:xs) = Just (x : x : xs)
-semCmd DUP _ = Nothing
-
--- Test cases
--- stack1 = [1, 2, 3, 4, 5]
--- test1 = [LD 3, DUP, ADD, DUP, MULT]
--- test2 = [LD 3, ADD]
--- test3 = []
--- test4 = [ADD, ADD, ADD, ADD]
+semCmd (LD n) s = Just (n : s)				-- Load int onto stack
+semCmd ADD (x:y:xs) = Just (x + y : xs)		-- Add top 2 ints on stack
+semCmd ADD _ = Nothing						-- If too few elements, return
+semCmd MULT (x:y:xs) = Just (x * y : xs)	-- Multiply top 2 ints on stack
+semCmd MULT _ = Nothing						-- If too few elements, return
+semCmd DUP (x:xs) = Just (x : x : xs)		-- Duplicate top element on stack
+semCmd DUP _ = Nothing						-- If stack is empty, return
--- Stacklang2.hs
+++ Stacklang2.hs
@@ -1,21 +1,41 @@
 module Stacklang2 where
 
+-- Define types for the program and the stack
 type Prog = [Cmd]
 type Stack = [Either Bool Int]
-data Cmd = LDI Int | LDB Bool | LEQ | ADD | MULT | DUP | IFELSE Prog Prog deriving Show
 
+-- Define the data type for the commands in the stack language
+data Cmd
+    = LDI Int			-- Load int onto stack
+    | LDB Bool			-- Load bool onto stack
+    | LEQ				-- Compare top 2 ints on stack, push result as bool
+    | ADD				-- Add top 2 ints on stack, push result
+    | MULT				-- Multiply top 2 integers on stack, push result
+    | DUP				-- Duplicate top element on stack
+    | IFELSE Prog Prog	-- If top element true, execute program 1, otherwise program 2
+    deriving Show
+
+-- Main function to execute a program on a given stack
 run :: Prog -> Stack -> Maybe Stack
-run [] s = Just s
+run [] s = Just s -- If no commands left, return stack
+-- Execute current command, continue with remaining commands
 run (c:cs) s = maybe Nothing (run cs) (semCmd c s)
 
+-- Function to execute individual commands
 semCmd :: Cmd -> Stack -> Maybe Stack
-semCmd (LDI n) s = Just (Right n : s)
-semCmd (LDB b) s = Just (Left b : s)
-semCmd ADD (Right x : Right y : xs) = Just (Right (x + y) : xs)
-semCmd MULT (Right x : Right y : xs) = Just (Right (x * y) : xs)
-semCmd DUP (x:xs) = Just (x:x:xs)
-semCmd LEQ (Right x : Right y : xs) = Just (Left (x <= y) : xs)
+semCmd (LDI n) s = Just (Right n : s)	-- Load int onto stack
+semCmd (LDB b) s = Just (Left b : s)	-- Load bool onto stack
+semCmd ADD (Right x : Right y : xs)		-- Add top 2 ints on stack
+    = Just (Right (x + y) : xs)
+semCmd MULT (Right x : Right y : xs)	-- Multiply top 2 ints on stack
+    = Just (Right (x * y) : xs)
+semCmd DUP (x:xs) = Just (x:x:xs)		-- Duplicate top element on stack
+semCmd LEQ (Right x : Right y : xs)		-- Compare top 2, push result as bool
+    = Just (Left (x <= y) : xs)
+
+-- Execute program 1 if top element is true, otherwise, execute program 2
 semCmd (IFELSE prog1 prog2) (Left b : xs) = if b
                                             then run prog1 xs
                                             else run prog2 xs
-semCmd _ _ = Nothing
+                                            
+semCmd _ _ = Nothing -- If invalid operation, return Nothing
